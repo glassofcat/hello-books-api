b912c3b8015b09e06086da66da2cd976e264d204
Jamie H <jamiewhorvath@gmail.com>
2022-05-05 12:37:48 -0400

    finally caught up and finished tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,11 +1,37 @@
-def test_get_all_books_with_no_records(client):
-    # Act
-    response = client.get("/books")
-    response_body = response.get_json()
+# def test_get_all_books_with_no_records(client):
+#     # Act
+#     response = client.get("/books")
+#     response_body = response.get_json()
 
-    # Assert
-    assert response.status_code == 200
-    assert response_body == []
+#     # Assert
+#     assert response.status_code == 200
+#     assert response_body == []
+
+# def test_get_one_book(client, two_saved_books):
+#     # Act
+#     response = client.get("/books/1")
+#     response_body = response.get_json()
+
+#     # Assert
+#     assert response.status_code == 200
+#     assert response_body == {
+#         "id": 1,
+#         "title": "Ocean Book",
+#         "description": "watr 4evr"
+#     }
+
+# def test_create_one_book(client):
+#     # Act
+#     response = client.post("/books", json={
+#         "title": "New Book",
+#         "description": "The Best!"
+#     })
+#     response_body = response.get_json()
+#     # response_body = response.get_data(as_text=True)
+
+#     # Assert
+#     assert response.status_code == 201
+#     assert response_body == "Book New Book successfully created"
 
 def test_get_one_book(client, two_saved_books):
     # Act
@@ -20,6 +46,30 @@ def test_get_one_book(client, two_saved_books):
         "description": "watr 4evr"
     }
 
+def test_get_from_empty_db(client):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+
+def test_get_all_book(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [    
+    {   "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"},
+    {   "id": 2,
+        "title": "Mountain Book",
+        "description":"i luv 2 climb rocks"}
+        ]
+
 def test_create_one_book(client):
     # Act
     response = client.post("/books", json={
@@ -27,6 +77,7 @@ def test_create_one_book(client):
         "description": "The Best!"
     })
     response_body = response.get_json()
+    # response_body = response.get_data(as_text=True)
 
     # Assert
     assert response.status_code == 201
